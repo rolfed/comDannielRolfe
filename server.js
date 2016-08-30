9d3efe3253a917c241f3ec0016ec13acfe14cd03
hrandp <danniel@hrandp.com>
2016-08-29 21:42:47 -0700

    Configured Node application server.js file

---
+++ server.js
@@ -0,0 +1,44 @@
+// Server.js
+
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
+// Configuration
+
+// config files
+var db = require('./config/db');
+
+
+// set port
+var port = process.env.PORT || 8000;
+
+// connect mondodb
+// mongoose.connect(db.url);
+
+// get all data/stuff of the body (post) parameters
+// parse application/json
+app.user(bodyParser.json());
+
+// parse application/vnd.api as JSON
+app.user(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// override with the X-HTTP-Method-Override header in the request. Simulate DELETE/PUT
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public' ));
+
+// routes
+require('./app/routes')(app); // configure our routes
+
+// start app
+// start our app http://localhost:8000
+app.listen(port);
+
+// shoutout to the user
+console.log('App is running on port ' + port + 'To terminate session type Ctrl-C');
+
+// expose app
+exports = module.export = app;
